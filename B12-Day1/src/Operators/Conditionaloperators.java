4317c240228fcc8611cc73a7651097c70f0300ba
bdurg <bdurg@LAPTOP-51JO8TA1>
2022-05-20 15:04:48 +0530

    first commit

---
+++ B12-Day1/src/Operators/Conditionaloperators.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Conditionaloperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5, y=4, z=7;  
+		System.out.println(x>y && x>z || y<z);  
+		System.out.println((x<z || y>z) && x<y);  
+
+	}
+
+}
---
+++ B12-Day1/src/Operators/arithmeticOperators.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class arithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3+45-56*48/59;
+      System.out.println(a);
+	}
+
+}
