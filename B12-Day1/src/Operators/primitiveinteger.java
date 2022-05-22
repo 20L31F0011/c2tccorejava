ab476481c0dc4e974fd1b9b4e8313b20aae32c58
bdurg <bdurg@LAPTOP-51JO8TA1>
2022-05-22 13:45:21 +0530

    first commit

---
+++ B12-Day1/src/Operators/primitiveinteger.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class primitiveinteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max = 2147483647;
+		int min = -2147483648;
+		short Max = 32767;
+	    short Min = -32768;
+		long  max= 9223372036854775807L;
+	    long  min= -9223372036854775808L;
+	     byteMax = 127;
+		 byteMin = -128;
+	}
+
+}
