892c9e7fcd53463c439d391f282ca3fe333ecfd7
bdurg <bdurg@LAPTOP-51JO8TA1>
2022-05-22 13:26:34 +0530

    first commit

---
+++ B12-Day1/src/Operators/Typecasting.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    int i= 10;  
+		       long l = (long) i; 
+		       System.out.println(l);
+			}
+}
+
+
+	
---
+++ B12-Day1/src/Operators/variables.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!"); //used to print something on ur console
+	}
+}
