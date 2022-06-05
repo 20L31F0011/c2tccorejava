5b7bba1baaf67963261b2bc9b376c7b50ea64ea2
bdurg <bdurg@LAPTOP-51JO8TA1>
2022-06-05 11:53:26 +0530

    first commit

---
+++ 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/Assignments2/src/programs/Rightangletriangle.java
@@ -0,0 +1,19 @@
+package programs;
+
+public class Rightangletriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		for(i = 1; i <= 4; i++)
+		{
+		    for(j = 1; j <= i; j++)  
+		    {          
+		        System.out.print(i); 
+		    }      
+		    System.out.print("\n");        
+		}
+
+	}
+
+}
