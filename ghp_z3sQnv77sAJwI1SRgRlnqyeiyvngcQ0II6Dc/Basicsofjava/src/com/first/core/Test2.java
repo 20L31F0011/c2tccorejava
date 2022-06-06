521815fe21003a367f28b226efdf2995d4b11e04
bdurg <bdurg@LAPTOP-51JO8TA1>
2022-06-06 10:04:00 +0530

    first commit

---
+++ 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/Basicsofjava/src/com/first/core/Test2.java
@@ -0,0 +1,20 @@
+package com.first.core;
+//program uses class Scanner-------1
+	import java.util.Scanner;
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner input=new Scanner(System.in);
+			
+			System.out.println("Enter First number: ");
+			
+			//read first number from user---3
+		       int num1 = input.nextInt();
+			
+			System.out.printf("number is %d %n",num1);
+		}
+
+	}
+
+
