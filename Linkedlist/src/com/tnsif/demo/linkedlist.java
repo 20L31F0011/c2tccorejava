8dd46ffa118c3763ec5af5212b8f25d0d3bdadf1
bdurg <bdurg@LAPTOP-51JO8TA1>
2022-06-11 20:41:14 +0530

    first commit

---
+++ Linkedlist/src/com/tnsif/demo/linkedlist.java
@@ -0,0 +1,19 @@
+package com.tnsif.demo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class linkedlist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> l1=new LinkedList<Integer>();
+        System.out.println("ENTER THE DATA:");
+             l1.add(4);
+             l1.add(5);
+             l1.add(6);
+             for(int i:l1)
+             {
+          	   System.out.println(i);
+             }
+             
